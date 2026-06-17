0313c58ca65bedc3deccbbbeef8661eb097c050c
Fangross <160512491+Fangross@users.noreply.github.com>
2026-06-16 22:53:13 -0400

    Add files via upload

---
+++ loot/SCRIPTS/rotatingplatform1.gd
@@ -0,0 +1,6 @@
+extends AnimatableBody2D
+
+
+
+func _process(delta: float) -> void:
+	rotation -= 1.8 * delta
---
+++ loot/SCRIPTS/rotatingplatform_2.gd
@@ -0,0 +1,5 @@
+extends AnimatableBody2D
+
+
+func _process(delta: float) -> void:
+	rotation += 1.8 * delta
