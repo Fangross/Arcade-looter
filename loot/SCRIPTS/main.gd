975b82caa9dbb913e76fe4874bc930f7c45ae269
Fangross <160512491+Fangross@users.noreply.github.com>
2026-06-18 11:36:09 -0400

    Add files via upload

--- loot/SCRIPTS/main.gd
+++ loot/SCRIPTS/main.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+const PLINKO = preload("res://SCENES/plinko.tscn")
+
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	
+	
+	
+	#TEMPORARY
+	var load_plinko = PLINKO.instantiate()
+	get_tree().current_scene.add_child(load_plinko)
+	
+	
 
-
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("esc"):
 		get_tree().quit()
