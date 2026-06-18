cf034eec7ae6619e1b88a9576e05fc28849b413f
Fangross <160512491+Fangross@users.noreply.github.com>
2026-06-18 00:47:30 -0400

    Add files via upload

--- loot/SCRIPTS/main.gd
+++ loot/SCRIPTS/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 
 func _input(event: InputEvent) -> void:
--- loot/SCRIPTS/rigid_body_2d.gd
+++ loot/SCRIPTS/rigid_body_2d.gd
@@ -1,20 +1,35 @@
 extends RigidBody2D
 
 var grabbed : bool = false
-var hand = false
+var can_grab = false
+@onready var multi: CollisionShape2D = $Area2D/CollisionShape2D
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	var hand = true
 
-func _process(_delta: float) -> void:
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			can_grab = true
+
+
+func _process(delta: float) -> void:
 	if grabbed:
 		var mouse_pos = get_global_mouse_position()
+		
+		
 		global_position = lerp(global_position,mouse_pos,0.2)
-		return
+		
 
 func _input(event: InputEvent) -> void:
 
-	if Input.is_action_pressed("lmb") and hand:
+	if Input.is_action_pressed("lmb") and can_grab:
 		grabbed = true
-	if Input.is_action_just_released("lmb") and hand:
+		multi.disabled = true
+
+	if Input.is_action_just_released("lmb") and can_grab:
 		grabbed = false
+		can_grab = false
+		multi.disabled = false
+		
+		
